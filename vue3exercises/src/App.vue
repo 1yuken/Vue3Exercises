665952368bc0fa4112cba154ca2253f63f4e9c3d
Osman Asanov <anilf.business@gmail.com>
2023-02-15 17:26:53 +0300

    025

--- vue3exercises/src/App.vue
+++ vue3exercises/src/App.vue
@@ -2,17 +2,21 @@
 export default {
   data() {
     return {
-      age: "Vue3 exercises",
+      visibility: true,
     }
   },
   methods: {
+    toggle: function() {
+      this.visibility = !this.visibility;
+    }
   }
 }
 </script>
 <template>
-  <h3>Enter age: </h3><br>
-  <input type="number" v-model.number="age">
-  <p v-if="age < 18">Подросток</p>
-  <p v-else-if="age < 25">Молодой человек</p>
-  <p v-else-if="age >= 25">Мужчина</p>
+  <div v-if="visibility">
+  <p>yuken</p>
+  <p>yuken2</p>
+  <p>yuken3</p>
+  </div><br>
+  <button @click="toggle">Toggle</button>
 </template>

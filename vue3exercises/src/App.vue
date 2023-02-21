2f44370f8c2a3eab9212b52c4fb131cc0b2f7b5b
Osman Asanov <anilf.business@gmail.com>
2023-02-21 15:09:05 +0300

    040

--- vue3exercises/src/App.vue
+++ vue3exercises/src/App.vue
@@ -2,9 +2,8 @@
 export default {
   data() {
     return {
-      obj: {
-        hidden: true,
-      },
+      isValid: true,
+      isDisabled: true,
     };
   },
   methods: {
@@ -15,14 +14,13 @@ export default {
 };
 </script>
 <template>
-  <button @click="this.obj.hidden = false">hide Task 2</button>
-  <button @click="this.obj.hidden = true">hide Task 3</button>
-  <button @click="setDone">{{ obj.hidden ? "show" : "hide" }} Task 4</button><br />
-  <p>&nbsp;&nbsp;&nbsp;&nbsp;</p>
-  <p :class="obj">Task 1</p>
+  <p :class="{ active: isValid, valid: isDisabled }">Task 1</p>
 </template>
 <style>
-p.hidden {
-  display: none;
+.active {
+  border: 5px solid red;
+}
+.valid {
+  color: green;
 }
 </style>

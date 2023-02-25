5a9b1cb73dbb6ed05fc7a87de8563cd3346061ca
Osman Asanov <anilf.business@gmail.com>
2023-02-25 13:43:32 +0300

    066

--- vue3exercises/src/components/Employee.vue
+++ vue3exercises/src/components/Employee.vue
@@ -1,22 +1,20 @@
 <script>
-export default {
-    emits: ['show', 'show1'],
-    data() {
-        return {
-        }
-    },
-    methods: {
-        handleName() {
-            this.$emit('show', 'Jujuba');
-        },
-        handleSalary(){
-            this.$emit('show1', 'Capybara', '20000');
-        }
-    }
+	export default {
+	emits: ['show', 'Jujuba', '20000    '],
+	data() {
+		return {
+			arg1: 3,
+			arg2: 4
+		}
+	},
+	methods: {
+		handle() {
+		this.$emit('show', 'Jujuba', '20000');
+	}
+}
 }
 </script>
-
 <template>
-<button class="button" @click="handleName">Name</button>
-<button class="button" @click="handleSalary">Salary</button>
+	
+	<button @click="handle">click</button>
 </template>

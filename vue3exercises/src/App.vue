5f036058deaad69e5a8ac91468a4654396ebead2
Osman Asanov <anilf.business@gmail.com>
2023-02-25 13:17:50 +0300

    054

--- vue3exercises/src/App.vue
+++ vue3exercises/src/App.vue
@@ -1,28 +1,29 @@
 <script>
-export default {
-  data() {
-    return {
-      newItem: "",
-      items: ["a", "b", "c", "d", "e"],
-    };
-  },
-  methods: {
-    addItemEnd: function () {
-      this.items.push(this.newItem);
-    },
-    addItemBegin: function () {
-      this.items.unshift(this.newItem);
-    },
-  },
-};
+export default{
+data(){
+  return{
+    newItem: '',
+    items: ['text', 'emran', 'down', 'text', 'text'],
+  }
+},
+methods: {
+  removeItem: function (index) {
+    this.items.splice(index, 1);
+  }
+}
+}
 </script>
+
 <template>
-  <input v-model="newItem" />
-  <button @click="addItemEnd">add to the end</button>
-  <button @click="addItemBegin">add to the beginning</button>
-  <ul v-for="(item, index) in items" :key="index">
-    <li>
-      {{ item }}
-    </li>
-  </ul>
+<ul class="styled">
+  <li v-for="(item, index) in 
+			items" :key="index">
+    {{ item }}
+    <br>
+    <button class="button" @click="removeItem(index)">remove</button>
+  </li>
+</ul>
 </template>
+
+<style scoped>
+</style>

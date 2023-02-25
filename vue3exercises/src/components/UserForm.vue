95827012165db953dc8b024d245f383305a4b7d9
Osman Asanov <anilf.business@gmail.com>
2023-02-25 13:53:31 +0300

    070

---
+++ vue3exercises/src/components/UserForm.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+    data() {
+        return {
+            newName: ' ',
+            newSurn: ' ',
+        }
+    },
+    methods: {
+        save(){
+            this.$emit('add', this.newName, this.newSurn)
+        }
+    }
+}
+</script>
+
+<template>
+    <input class="text-field__input" v-model="newName">
+    <br>
+    <br>
+    <input class="text-field__input" v-model="newSurn">
+    <br>
+    <button class="button" @click="save">save</button>
+</template>

46a273a4d96b89372f8bc79608b93db5b0f84811
Osman Asanov <anilf.business@gmail.com>
2023-02-25 13:39:22 +0300

    064

--- vue3exercises/src/components/Employee.vue
+++ vue3exercises/src/components/Employee.vue
@@ -1,21 +1,22 @@
 <script>
 export default {
-    props: {
-        name: String, 
-        salary: Number, 
-        age: Number
-    },
+    emits: ['show', 'show1'],
     data() {
         return {
         }
+    },
+    methods: {
+        handle() {
+            this.$emit('show');
+        },
+        handle1() {
+            this.$emit('show1');
+        },
     }
 }
 </script>
 
 <template>
-<p class="user">{{ name }} 
-<br>
-{{ salary }} 
-<br>
-{{ age }}</p>
+<button class="button" @click="handle">jujuba</button>
+<button class="button" @click="handle1">capybara</button>
 </template>
